611cc76e4ee4ef3e42f0584caf26be926564035e
Dimitrii Ustinov <160676624+dimitrii-ustinov@users.noreply.github.com>
2024-05-12 20:09:56 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify
+import pandas as pd
+import requests
 
 application = Flask(__name__)
 
@@ -25,6 +27,50 @@ for i in range(200):
 @application.route("/")
 def get_aqi_data():
     return jsonify(aqi_data)
+
+# Construct the API URL with parameters
+url = "https://gis.ccc.govt.nz/server/rest/services/OpenData/Furniture/FeatureServer/19/query?where=1%3D1&outFields=Type,PhotographURL,SiteName,AssetLongDescription,ContractArea,ServiceStatus&outSR=4326&f=json"
+params = {
+    'where': '1=1',
+    'outFields': 'Type,PhotographURL,SiteName,AssetLongDescription,ContractArea,ServiceStatus',
+    'outSR': '4326',
+    'f': 'json'
+}
+
+# Make the GET request
+def get_df():
+    response = requests.get(url, params=params)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        
+        # Extract the features from the response
+        features = data.get('features', [])
+        #print(features[10])
+        
+        # Extract attributes from each feature
+        records = [feature['attributes'] for feature in features]
+        geometry = [feature['geometry'] for feature in features]
+        
+        # Create DataFrame
+        df = pd.DataFrame(records)
+        for values in geometry:
+            df['lon'] = values['x']
+            df['lat'] = values['y']
+
+        #df["Last_Edit_Date"] = pd.to_datetime(df["LastEditDate"], unit='ms').dt.date
+        #del(df["LastEditDate"])
+        return df
+    else:
+        print("Error:", response.status_code)
+
+@app.route('/data')
+def get_data():
+    df = get_df()
+    # Return the entire DataFrame as JSON
+    return jsonify(df.to_dict(orient='records'))
 
 
 if __name__ == "__main__":
